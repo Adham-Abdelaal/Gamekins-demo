4af318df6d93a581e9e656439933a8c6c82c3370
admin <2100725@eng.asu.edu.eg>
2025-12-11 21:37:52 +0200

    Initial commit

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // This method is the "Trap". We will intentionally NOT test this initially.
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
